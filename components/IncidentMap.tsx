1dfbd1219544f708312674305de0dd659362aa7c
Mohamed sabek <147674690+msabek@users.noreply.github.com>
2024-08-05 14:53:51 -0600

    Update IncidentMap.tsx

--- components/IncidentMap.tsx
+++ components/IncidentMap.tsx
@@ -60,14 +60,30 @@ const IncidentMap: React.FC<IncidentMapProps> = ({ incidents, onAddIncident }) =
   return (
     <MapContainer center={[52.1332, -106.6700]} zoom={13} style={{ height: '400px', width: '100%' }}>
       <LayersControl position="topright">
-        {/* Base layers and overlays remain the same */}
         <LayersControl.BaseLayer checked name="OpenStreetMap">
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
         </LayersControl.BaseLayer>
-        {/* ... other base layers ... */}
+        <LayersControl.BaseLayer name="Satellite">
+          <TileLayer
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+          />
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="Topographic">
+          <TileLayer
+            url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
+            attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+          />
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="Humanitarian">
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+          />
+        </LayersControl.BaseLayer>
 
         <LayersControl.Overlay checked name="Labels">
           <TileLayer
